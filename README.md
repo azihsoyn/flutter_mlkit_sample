3169dec705161d506c4177126ff437ada566ec86
azihsoyn <azihsoyn@gmail.com>
2018-06-08 23:19:26 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_mlkit_sample
+
+sample app for [flutter_mlkit](https://github.com/azihsoyn/flutter_mlkit)
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ android/app/src/main/java/mlkit/sample/flutter/azihsoyn/com/fluttermlkitsample/MainActivity.java
@@ -0,0 +1,14 @@
+package mlkit.sample.flutter.azihsoyn.com.fluttermlkitsample;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
